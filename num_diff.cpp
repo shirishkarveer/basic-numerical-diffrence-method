85319557662e376f3814f77b8ad36919c7343a2e
shirishkarveer <33382424+shirishkarveer@users.noreply.github.com>
2023-11-11 15:18:53 -0800

    Update num_diff.cpp

--- num_diff.cpp
+++ num_diff.cpp
@@ -15,6 +15,10 @@ DO THE DIFFRENTIATION USING EQUATION
 COUT << RESULT
 y = A+Bx+Cx^2+....EX^4+… (10 DEGREE POLYNOMIAL = 11 COEFFS, 4+3+2+1+1=11 )
 y=coef[0]+coef[1]*X^1+....
+1.write the main function 
+2.void displayPOLYNOMIAL
+3.float POLYNOMIAL
+4.LINE 68 ONWARDS math diffrentiation
 */
 
 void displayPOLYNOMIAL(float coeff[11], int degree) {  //sub-function so that main is not heavy
@@ -35,7 +39,7 @@ float POLYNOMIAL(float coeff[11], float degree, float X ){// creating a sub-fn m
 	float Y = 0.0;
 	for (int i = 0; i <= degree; i++)
 	{
-		Y += coeff[i] * pow(X, i);		//as per eqn y = A+Bx+...
+		Y += coeff[i] * pow(X, i);		//as per eqn y = A+Bx+..., anything to pow 1 = 0
 	}
 	cout << "Y =" << Y << endl;			//
 	return Y;
@@ -50,22 +54,22 @@ int main()
 
 	float coeff[11];		//BECAUSE 10 DEGREE POLYNOMIAL so 11 inside the ARRAY
 	cout << "Input POLYNOMIAL constants:" << endl;		//user input for how many constants 
-	for (int i = 0; i <= degree; i++) {   //
-		cout << "coeff" << i << ":";			//each one should print out coeff#
-		cin >> coeff[i];		//need to tell program which co-eff like A = coeff 0, B = coeff 1...
+	for (int i = 0; i <= degree; i++) {     //rest further co-effs would be zero 
+		cout << "coeff" << i << ":";	//each one should print out coeff#
+		cin >> coeff[i];		//need to tell program which co-eff like A = coeff 0, B = coeff 1, C = 2...
 	}
 	
-	displayPOLYNOMIAL(coeff, degree); // to displayp POLYNOMIALnomial
+	displayPOLYNOMIAL(coeff, degree); // to display POLYNOMIAL
 	 
-	float X;		//contruct the POLYNOMIALnomial
+	float X;		//contruct the POLYNOMIAL
 	cout << "input value for X for diff: ";
 	cin >> X;
 
 	float y = POLYNOMIAL(coeff, degree, X);//
-	float h = 0.0001; // need to choose the approriate h  value as lt.X->0, CAN EXPERIMENT WITH THIS VALUE, CURRENT VALUE IS MOST OPTIMAL
-	float X1 = X + h; 
-	float y1 = POLYNOMIAL(coeff, degree, X1); 
+	float h = 0.0001; // need to choose the approriate tiny h  value as lt.X->0, CAN EXPERIMENT WITH THIS VALUE, CURRENT VALUE IS MOST OPTIMAL
+	float X1 = X + h; //X+h = distance on Y -> axis
+	float y1 = POLYNOMIAL(coeff, degree, X1); //distance on X -> axis
 
-	float slope = (y1 - y) / h; //optimization for consistent slope for Convergence of FD 
+	float slope = (y1 - y) / h; //optimization for consistent slope (meaning differentiation) for Convergence of FD , see line 70
 	cout << "slope = " << slope << endl;	
 }
